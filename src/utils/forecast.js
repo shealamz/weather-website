2070f89961c18080ade67a7143d1b9d2d86796ae
shealamz <shealamz@gmail.com>
2019-10-20 10:55:33 +0700

    Init commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,18 @@
+const request = require('request')
+const forecast = (latitude, longitude,  callback) => {
+    const url = "https://api.darksky.net/forecast/358a819e2fa0a5f618b6c50d25e71409/"+latitude+","+longitude+"?lang=id";
+
+    request({url, json: true}, (error, { body }) => {
+    console.log(body.latitude,body.longitude)
+        if(error){
+            callback('Harus terhubung Internet', undefined)
+        }else if(body.code){
+            callback('Kordinat salah', undefined)
+        }else{
+            callback(undefined, body.daily.data[0].summary+" Perkiraan suhu saat ini "+ body.currently.temperature + " derajat. Ada kemungkinan " + body.currently.precipProbability + "% hujan" 
+            )
+        }
+    })
+}
+
+module.exports = forecast
